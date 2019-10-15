a37b343ff1d5f01800681ef7fb280bbd068140a4
BussardRamjet <norszabo@gmail.com>
2019-10-14 23:01:33 -0700

    adding placeholder window

---
+++ src/companion.cpp
@@ -0,0 +1,10 @@
+#include "companion.h"
+#include "imgui.h"
+
+void companion_draw()
+{
+	ImGui::Begin("Companion");                          
+	ImGui::Text("Welcome to Mattel DnD Portable Companion!");
+	ImGui::End();
+
+}
---
+++ src/companion.h
@@ -0,0 +1,3 @@
+#pragma once
+
+void companion_draw();
